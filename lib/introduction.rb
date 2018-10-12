dcdde830ad0cb556389fd1c50f78c72f56015640
tjaymenchat <tjaymenchat@gmail.com>
2018-10-12 02:02:59 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def name_with_language(name="Dan", language="Ruby")
-  puts "Hi, i'm #{name} and im learning programing in #{language}."
+def introduction_with_language(introduction="Dan", language="Ruby")
+  puts "Hi, i'm #{introduction} and im learning programing in #{language}."
 end
   
