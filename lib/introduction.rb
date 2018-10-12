8d3bd88325020f553f1063f2514d2b4b938b5e7c
tjaymenchat <tjaymenchat@gmail.com>
2018-10-12 02:55:20 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(introduction="Dan", language="Ember")
+  puts "Hi, my name is #{introduction} and I am learning to program in #{language}."
+end
+  
